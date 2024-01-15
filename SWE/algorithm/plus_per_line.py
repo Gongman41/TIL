4c4b6cdf5423a5044f32831b9a0207c2f94f5d1e
Gongman41 <lkm98401@gmail.com>
2024-01-15 17:49:20 +0900

    파이썬 1일차 끝

--- SWE/algorithm/plus_per_line.py
+++ SWE/algorithm/plus_per_line.py
@@ -1,12 +1,33 @@
 T = int(input())
-for t in T:
+for t in range(T):
     numbers = list(map(int,input().split()))
     A_number = numbers[0]
     B_number = numbers[1]
     A = list(map(int,input().split()))
     B = list(map(int,input().split()))
+    real_max_number = 0
     max_number = 0
     if A_number <= B_number:
-        for i in (B_number - A_number + 1):
-            ...
-        
+        for i in range(B_number - A_number + 1):
+            prev_max_number = real_max_number
+            max_number = 0  
+
+            for j in range(A_number):
+                max_number += A[j] * B[j+i]
+            if max_number >= prev_max_number:
+                real_max_number = max_number
+            else:
+                real_max_number = prev_max_number
+    else:
+        for i in range(A_number - B_number + 1):
+            prev_max_number = real_max_number
+            max_number = 0
+
+            for j in range(B_number):
+                max_number += A[j+i] * B[j]
+            if max_number >= prev_max_number:
+                real_max_number = max_number
+            else:
+                real_max_number = prev_max_number
+
+    print(f"#{t+1} {real_max_number}")
---
+++ SWE/algorithm/turn_arrange_n.py
@@ -0,0 +1,7 @@
+T = int(input())
+for t in range(T):
+    N = int(input())
+    number_list = []
+    for i in range(N):
+        number_list.append(list(map(int.input().split())))
+    
