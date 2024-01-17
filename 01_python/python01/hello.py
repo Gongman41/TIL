66e8ab3327e2d3d731e8c351da2d37354b720ebd
Gongman41 <lkm98401@gmail.com>
2024-01-17 17:46:02 +0900

    4_2 왜 어렵냐

--- 01_python/python01/hello.py
+++ 01_python/python01/hello.py
@@ -1,35 +1,6 @@
-T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    N, M = map(int, input().split())
-    number_list = []
-    real_total_kill = 0
-
-    for n in range(N):
-        number_list.append(list(map(int, input().split())))
-
-    for i in range(N):
-        for j in range(N):
-            total_kill = 0
-
-            # 가로 및 세로 합
-            for k in range(-M + 1, M):
-                if 0 <= i + k < N:
-                    total_kill += number_list[i + k][j]
-                if 0 <= j + k < N:
-                    total_kill += number_list[i][j + k]
-
-            # 대각선 합
-            for k in range(-M + 1, M):
-                if 0 <= i + k < N and 0 <= j + k < N:
-                    total_kill += number_list[i + k][j + k]
-                if 0 <= i - k < N and 0 <= j + k < N:
-                    total_kill += number_list[i - k][j + k]
-
-            # 현재 셀의 값을 두 번 더했으므로 중복 계산된 값을 빼줍니다.
-            total_kill -= 2 * number_list[i][j]
-
-            real_total_kill = max(real_total_kill, total_kill)
-
-    print(f'#{test_case}')
-    print(real_total_kill)
+def other_fuc(parm1, parm2):
+    result = parm1 * parm2
+    print(result, '함수 내부에서 실행')
+    return result
+answer = other_fuc(2,3)
+print(answer, '함수 외부에서 실행')
--- information.md
+++ information.md
@@ -1,2 +1,10 @@
 ## 주피터 노트북 사용방법
 - pip install notebook
+
+- vim ~/.bashrc
+
+- alias nb="jupyter notebok"
+ 별칭 앞으로 이렇게 쓸거야
+
+-  source ~/.bashrc
+-  nb
