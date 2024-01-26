d744488e259038ccc47d9e0148ec3f83cb2005ff
Gongman41 <lkm98401@gmail.com>
2024-01-26 17:38:50 +0900

    주말에 못쉬겠네

--- 00_startcamp/startcamp02/practice.py
+++ 00_startcamp/startcamp02/practice.py
@@ -1,4 +1,13 @@
-N = 6789
-a = N/1000
+import sys
 
-print(a)
+N = int(sys.stdin.readline().rstrip())
+n_list = [0] * 10001
+
+for m in range(N):
+    num = int(sys.stdin.readline().rstrip())
+    n_list[int(num)] += 1
+
+for n in range(len(n_list)):
+    if n_list[n] != 0:
+        for i in range(n_list[n]):
+            print(n)
