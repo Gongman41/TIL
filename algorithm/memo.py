7df61af34af2b01e0353d1a0afa2e789785bffa7
Gongman41 <lkmn98401@gmail.com>
2024-02-01 17:53:13 +0900

    아프다

--- algorithm/memo.py
+++ algorithm/memo.py
@@ -1,11 +1,25 @@
+N, K = map(int, input().split())
+share_ncard = list(map(int, input().split()))
+team_ncard = list(map(int, input().split()))
 
-T = int(input())
-for test_case in range(1,T+1):
-    N = int(input())
-    n_list = list(map(int,input().split()))
-    max_n = max(n_list)
-    min_n = min(n_list)
-    print(f"#{test_case} {max_n - min_n}")
+max_n_list = []
+for s in range(len(share_ncard)):
+    for t in range(len(team_ncard)):
+        max_n_list.append([share_ncard[s] * team_ncard[t], t])
+max_n_list = sorted(max_n_list, key=lambda x: x[0])
+for k in range(K):
+    a = max_n_list.pop()
+    team_ncard.pop(a[1])
+    i = 0
+    while i != len(max_n_list):
+        if max_n_list[i][1] == a[1]:
+            max_n_list.pop(i)
+        else:
+            i += 1
+
+print(max(team_ncard) * max(share_ncard))
+
+
 
 
 
