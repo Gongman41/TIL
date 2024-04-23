83ee95384fbc996878f7160efb04fd239395a3f6
Gongman41 <lkm98401@gmail.com>
2024-04-24 08:18:42 +0900

    d

---
+++ TWL/New_Buwool_1주차/10998_별찍기-22/gong.py
@@ -0,0 +1,25 @@
+def star(n):
+    if n == 1:
+        return
+    elif n == 2:
+        lst = ['*****','*    ','* ***','* * *','* * *','*   *','*****']
+        return lst
+    else:
+        last_star = star(n-1)
+        this_star = ['*'*(4*n-3),'*'+' '*(4*n-4)]
+        
+        for i in range(len(last_star)):
+            if i == 0:
+                last_star[i] = '* ' + last_star[i] + '**'
+            else:
+                last_star[i] = '* ' + last_star[i] + ' *'
+            
+        this_star = this_star + last_star +['* '+' '*(4*n-7)+' *','*'*(4*n-3)]
+        return this_star
+N = int(input())
+if N == 1:
+    print('*')
+else:
+    stars = star(N)
+    for starr in stars:
+        print(starr)
